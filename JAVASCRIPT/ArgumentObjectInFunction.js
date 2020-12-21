7b1d5a184efd9d901f279d03f46ac139c1359f96
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2020-12-21 20:23:11 +0530

    argument object

--- JAVASCRIPT/ArgumentObjectInFunction.js
+++ JAVASCRIPT/ArgumentObjectInFunction.js
@@ -7,9 +7,9 @@ for(i=0;i<arguments.length;i++)
 }
 return sum;
 }
-var a=fun(1,2,3);
-var b=fun(4,5 ,6 ,7 ,23,22);
-var c=fun(1,4,6,3,7,22,344,5,789,4,5,6,7,8,9);
+var a=fun(1,2,3);                               //6
+var b=fun(4,5 ,6 ,7 ,23,22);                    //67
+var c=fun(1,4,6,3,7,22,344,5,789,4,5,6,7,8,9);   //1220
 console.log(a);
 console.log(b);
 console.log(c);
